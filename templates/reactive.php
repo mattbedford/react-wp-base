aac8e193da27c74136333f769fee7b5a3f1b9f62
mattbedford <matt.bedford@gmail.com>
2025-01-17 13:29:46 +0100

    Add logged-in protection

--- templates/reactive.php
+++ templates/reactive.php
@@ -2,6 +2,11 @@
 
 do_action('wp_head');
 
+
+if(!current_user_can('edit_posts') && !current_user_can('edit_pages')) {
+    die("You don't have permission to view this page.");
+}
+
 echo "<div>";
 echo "<div id='react-app'>";
 echo "</div>";
